0ad4d95fb8fb7fd12bbbc9fac498066823e6a156
David Rodrigues Fernandes <davidro2@c1r1s6.42lisboa.com>
2023-11-07 12:10:29 +0000

    07/11 12

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: davidro2 <davidro2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 14:04:58 by davidro2          #+#    #+#             */
-/*   Updated: 2023/10/26 15:26:39 by davidro2         ###   ########.fr       */
+/*   Updated: 2023/11/07 11:36:13 by davidro2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	ft_lstsize(t_list *lst)
 	i = 0;
 	while (lst)
 	{
-		i++;
 		lst = lst->next;
+		i++;
 	}
 	return (i);
 }
